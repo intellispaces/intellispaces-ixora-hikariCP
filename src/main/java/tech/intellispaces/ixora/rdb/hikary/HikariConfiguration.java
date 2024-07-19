b3688ba5ada618380cb73eafc7f6b5da9d16799c
lavor <lavor.andrey@gmail.com>
2024-07-19 16:52:30 +0600

    Minor changes

--- src/main/java/tech/intellispaces/ixora/rdb/hikary/HikariConfiguration.java
+++ src/main/java/tech/intellispaces/ixora/rdb/hikary/HikariConfiguration.java
@@ -3,7 +3,6 @@ package tech.intellispaces.ixora.rdb.hikary;
 import intellispaces.ixora.rdb.DataSourcePropertiesHandle;
 import tech.intellispaces.framework.core.annotation.Configuration;
 import tech.intellispaces.framework.core.annotation.Projection;
-import tech.intellispaces.ixora.rdb.hikary.HikariDataSource;
 
 @Configuration
 public class HikariConfiguration {
