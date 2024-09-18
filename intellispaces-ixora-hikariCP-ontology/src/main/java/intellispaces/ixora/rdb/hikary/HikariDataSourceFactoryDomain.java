07e5627da7164c40d89f9ee9395d63339f936812
lavor <lavor.andrey@gmail.com>
2024-09-18 15:24:16 +0600

    Flag factory of annotation @Transition removed

--- intellispaces-ixora-hikariCP-ontology/src/main/java/intellispaces/ixora/rdb/hikary/HikariDataSourceFactoryDomain.java
+++ intellispaces-ixora-hikariCP-ontology/src/main/java/intellispaces/ixora/rdb/hikary/HikariDataSourceFactoryDomain.java
@@ -9,6 +9,6 @@ import intellispaces.framework.core.traverse.TraverseTypes;
 @Domain("7aae1f0c-1d54-43a3-a9f8-fc32bbb6891e")
 public interface HikariDataSourceFactoryDomain {
 
-  @Transition(value = "16b62a7e-4c8c-4d41-bd44-36581db13589", allowedTraverse = TraverseTypes.Moving, factory = true)
+  @Transition(value = "16b62a7e-4c8c-4d41-bd44-36581db13589", allowedTraverse = TraverseTypes.MovingThenMapping)
   HikariDataSourceDomain create(HikariDataSourcePropertiesDomain properties);
 }
