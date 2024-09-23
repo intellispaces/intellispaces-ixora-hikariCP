b98ce4e0259be155403950b8062d3835a00146d8
lavor <lavor.andrey@gmail.com>
2024-09-23 09:19:55 +0600

    TraverseTypes.MovingThenMapping renamed to MappingRelatedToMoving

--- intellispaces-ixora-hikariCP-ontology/src/main/java/intellispaces/ixora/rdb/hikary/HikariDataSourceFactoryDomain.java
+++ intellispaces-ixora-hikariCP-ontology/src/main/java/intellispaces/ixora/rdb/hikary/HikariDataSourceFactoryDomain.java
@@ -5,10 +5,10 @@ import intellispaces.framework.core.annotation.Factory;
 import intellispaces.framework.core.annotation.Transition;
 import intellispaces.framework.core.traverse.TraverseTypes;
 
-@Factory
 @Domain("7aae1f0c-1d54-43a3-a9f8-fc32bbb6891e")
 public interface HikariDataSourceFactoryDomain {
 
-  @Transition(value = "16b62a7e-4c8c-4d41-bd44-36581db13589", allowedTraverse = TraverseTypes.MovingThenMapping)
+  @Factory
+  @Transition(value = "16b62a7e-4c8c-4d41-bd44-36581db13589", allowedTraverse = TraverseTypes.MappingRelatedToMoving)
   HikariDataSourceDomain create(HikariDataSourcePropertiesDomain properties);
 }
