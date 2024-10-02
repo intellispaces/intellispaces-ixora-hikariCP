f8113d233f9a10cb50fb88f3ae8f867bd22a98ab
lavor <lavor.andrey@gmail.com>
2024-10-02 11:13:55 +0600

    Transitions renamed to channels

--- intellispaces-ixora-hikariCP-ontology/src/main/java/intellispaces/ixora/rdb/hikary/HikariDataSourceDomain.java
+++ intellispaces-ixora-hikariCP-ontology/src/main/java/intellispaces/ixora/rdb/hikary/HikariDataSourceDomain.java
@@ -1,12 +1,12 @@
 package intellispaces.ixora.rdb.hikary;
 
+import intellispaces.framework.core.annotation.Channel;
 import intellispaces.framework.core.annotation.Domain;
-import intellispaces.framework.core.annotation.Transition;
 import intellispaces.ixora.rdb.DataSourceDomain;
 
 @Domain("01908c4f-942b-7715-a5a0-0d89c9aaebaf")
 public interface HikariDataSourceDomain extends DataSourceDomain {
 
-  @Transition("4167089f-3759-4187-85fb-5b324fcc2565")
+  @Channel("4167089f-3759-4187-85fb-5b324fcc2565")
   HikariDataSourcePropertiesDomain properties();
 }
--- intellispaces-ixora-hikariCP-ontology/src/main/java/intellispaces/ixora/rdb/hikary/HikariDataSourceFactoryDomain.java
+++ intellispaces-ixora-hikariCP-ontology/src/main/java/intellispaces/ixora/rdb/hikary/HikariDataSourceFactoryDomain.java
@@ -1,14 +1,14 @@
 package intellispaces.ixora.rdb.hikary;
 
+import intellispaces.framework.core.annotation.Channel;
 import intellispaces.framework.core.annotation.Domain;
 import intellispaces.framework.core.annotation.Factory;
-import intellispaces.framework.core.annotation.Transition;
 import intellispaces.framework.core.traverse.TraverseTypes;
 
 @Domain("7aae1f0c-1d54-43a3-a9f8-fc32bbb6891e")
 public interface HikariDataSourceFactoryDomain {
 
   @Factory
-  @Transition(value = "16b62a7e-4c8c-4d41-bd44-36581db13589", allowedTraverse = TraverseTypes.MappingOfMoving)
+  @Channel(value = "16b62a7e-4c8c-4d41-bd44-36581db13589", allowedTraverse = TraverseTypes.MappingOfMoving)
   HikariDataSourceDomain create(HikariDataSourcePropertiesDomain properties);
 }
