28354b088fe6b9ef623af9c8f3762e3b7cdd3e65
lavor <lavor.andrey@gmail.com>
2024-10-18 11:15:30 +0600

    Remove attribute targetSpecifications in annotation Channel

--- intellispaces-ixora-hikariCP-ontology/src/main/java/intellispaces/ixora/rdb/hikary/HikariDataSourceFactoryDomain.java
+++ intellispaces-ixora-hikariCP-ontology/src/main/java/intellispaces/ixora/rdb/hikary/HikariDataSourceFactoryDomain.java
@@ -3,7 +3,7 @@ package intellispaces.ixora.rdb.hikary;
 import intellispaces.framework.core.annotation.Channel;
 import intellispaces.framework.core.annotation.Domain;
 import intellispaces.framework.core.annotation.Factory;
-import intellispaces.framework.core.annotation.TargetSpecification;
+import intellispaces.framework.core.annotation.Movable;
 import intellispaces.framework.core.traverse.TraverseTypes;
 
 @Domain("7aae1f0c-1d54-43a3-a9f8-fc32bbb6891e")
@@ -12,8 +12,8 @@ public interface HikariDataSourceFactoryDomain {
   @Factory
   @Channel(
       value = "16b62a7e-4c8c-4d41-bd44-36581db13589",
-      allowedTraverse = TraverseTypes.MappingOfMoving,
-      targetSpecifications = TargetSpecification.Movable
+      allowedTraverse = TraverseTypes.MappingOfMoving
   )
+  @Movable
   HikariDataSourceDomain create(HikariDataSourcePropertiesDomain properties);
 }
