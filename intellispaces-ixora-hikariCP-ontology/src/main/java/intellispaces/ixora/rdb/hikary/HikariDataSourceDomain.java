e168d477d337de76117170ebf6dd56598f2384a0
lavor <lavor.andrey@gmail.com>
2024-10-20 15:09:07 +0600

    Update

--- intellispaces-ixora-hikariCP-ontology/src/main/java/intellispaces/ixora/rdb/hikary/HikariDataSourceDomain.java
+++ intellispaces-ixora-hikariCP-ontology/src/main/java/intellispaces/ixora/rdb/hikary/HikariDataSourceDomain.java
@@ -7,6 +7,9 @@ import intellispaces.ixora.rdb.DataSourceDomain;
 @Domain("01908c4f-942b-7715-a5a0-0d89c9aaebaf")
 public interface HikariDataSourceDomain extends DataSourceDomain {
 
+  @Channel("29dff81b-c679-49e2-9f0a-60e24ecfc342")
+  DataSourceDomain asDataSource();
+
   @Channel("4167089f-3759-4187-85fb-5b324fcc2565")
   HikariDataSourcePropertiesDomain properties();
 }
--- intellispaces-ixora-hikariCP-ontology/src/main/java/intellispaces/ixora/rdb/hikary/HikariDataSourcePropertiesDomain.java
+++ intellispaces-ixora-hikariCP-ontology/src/main/java/intellispaces/ixora/rdb/hikary/HikariDataSourcePropertiesDomain.java
@@ -1,5 +1,6 @@
 package intellispaces.ixora.rdb.hikary;
 
+import intellispaces.framework.core.annotation.Channel;
 import intellispaces.framework.core.annotation.Data;
 import intellispaces.framework.core.annotation.Domain;
 import intellispaces.ixora.rdb.DataSourcePropertiesDomain;
@@ -7,5 +8,8 @@ import intellispaces.ixora.rdb.DataSourcePropertiesDomain;
 @Data
 @Domain("3094f25e-798b-4bf9-a84c-7e581522060a")
 public interface HikariDataSourcePropertiesDomain extends DataSourcePropertiesDomain {
+
+  @Channel("c6a1f862-1752-4e60-acf6-880c29cfd727")
+  DataSourcePropertiesDomain asDataSourceProperties();
 }
 
